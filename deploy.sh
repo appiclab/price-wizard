cd32afcd931d50560c8faff5c0737f262a975d93
Stelios Baglaridis <stelios@baglaridis.com>
2021-01-21 21:32:33 +0200

    update the settings for deploy

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git add -A
 git commit -m 'deploy'
 
 ## if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/appiclab/seo-tool.git master:gh-pages
+git push -f https://github.com/appiclab/price-wizard.git master:gh-pages
 
 ## if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f https://<USERNAME>.github.io/<REPO> master:gh-pages
